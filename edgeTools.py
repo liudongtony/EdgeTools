8f7ec8ed24aa6d0fb8caab2b61b84f440ad95112
tony-quantil <liudongtony@gmail.com>
2017-02-15 17:07:13 -0800

    added user()

--- edgeTools.py
+++ edgeTools.py
@@ -9,5 +9,9 @@ def hello_world():
     return '<h1>Hello World!</h1>'
 
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello %s!</h1>' %name
+
 if __name__ == '__main__':
     app.run()
