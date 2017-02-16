c5c0e6519fae626c84582338a4576b5c55411eea
tony-quantil <liudongtony@gmail.com>
2017-02-15 23:22:12 -0800

    add js

--- edgeTools.py
+++ edgeTools.py
@@ -4,9 +4,11 @@ from flask import make_response
 from flask import redirect
 from flask import abort
 from flask import render_template
+from datetime import datetime
 
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 import configs
 
@@ -16,10 +18,11 @@ app.debug = True
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
@@ -56,6 +59,16 @@ def find_user(id):
     if not user:
         abort(404)
     return '<h1>Hello %s!</h1>' %user
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
 
 if __name__ == '__main__':
     manager.run()
