2447273ce768d29850e5e3a600de57180d0dd8ab
tony-quantil <liudongtony@gmail.com>
2017-02-17 22:51:54 -0800

    add readme.md

--- edgeTools.py
+++ edgeTools.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from threading import Thread
+import os
 
 from flask import Flask
 from flask import abort
@@ -11,21 +13,60 @@ from flask import url_for
 from flask import flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_script import Manager
+from flask_script import Manager, Shell, Server
 from flask_wtf import Form
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 import configs
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.debug = True
+# app.debug = True
 app.config['SECRET_KEY'] = 'This is a secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['EDGETOOLS_MAIL_SUBJECT_PREFIX'] = '[EdgeTools]'
+app.config['EDGETOOLS_MAIL_SENDER'] = 'EdgeTools Admin <testofdong@gmail.com>'
+app.config['EDGETOOLS_ADMIN'] = os.environ.get('EDGETOOLS_ADMIN')
 
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+mail = Mail(app)
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command('runserver', Server(use_debugger=True))
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['EDGETOOLS_MAIL_SUBJECT_PREFIX'] + subject, sender=app.config['EDGETOOLS_MAIL_SENDER'],
+                  recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 
 class NameForm(Form):
@@ -33,21 +74,50 @@ class NameForm(Form):
     submit = SubmitField('Submit')
 
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    user = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            if app.config['EDGETOOLS_ADMIN']:
+                send_email(app.config['EDGETOOLS_ADMIN'], 'New User', 'mail/new_user', user=user)
+        else:
+            session['known'] = True
+            # flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'),
+                           known=session.get('known', False))
 
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+@app.route('/auser/<name>')
+def auser(name):
+    return render_template('auser.html', name=name)
 
 
 @app.route('/headers')
@@ -75,7 +145,7 @@ def make_redirect():
 
 @app.route('/users/<id>')
 def find_user(id):
-    user=configs.users[id]
+    user = configs.users[id]
     if not user:
         abort(404)
     return '<h1>Hello %s!</h1>' %user
